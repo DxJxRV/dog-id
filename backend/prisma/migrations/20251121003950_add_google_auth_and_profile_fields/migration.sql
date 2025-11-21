bd77347cc1646c63ee6c633ef4df55908b8740b9
DxJx <diegovalentin663@gmail.com>
2025-11-21 00:40:51 -0600

    [add] Migracion google id

---
+++ backend/prisma/migrations/20251121003950_add_google_auth_and_profile_fields/migration.sql
@@ -0,0 +1,10 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `google_id` VARCHAR(191) NULL,
+    ADD COLUMN `cover_photo_url` VARCHAR(191) NULL,
+    ADD COLUMN `telefono` VARCHAR(191) NULL;
+
+-- AlterTable
+ALTER TABLE `vets` ADD COLUMN `cover_photo_url` VARCHAR(191) NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `users_google_id_key` ON `users`(`google_id`);
